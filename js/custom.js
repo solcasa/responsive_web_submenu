e0bda83d16e10ea9718989b05e00eefd869764e3
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-07-20 18:07:30 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 $(function () {
-    $('.header .gnb>ul>li').on('click', function (e) {
+    $('.header .gnb>ul>li>a').on('click', function (e) {
         if ($('.header .gnb').hasClass('on')) {
             // if문 붙여주면 모바일on. 에서만 작동해랏
             e.preventDefault();
-            $(this).find('.submenu').stop().slideToggle();
+            $(this).next().stop().slideToggle();
         }
     });
 
